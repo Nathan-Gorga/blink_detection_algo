3c5c8f2bbd1e46fa4be8967a86ad7a02c2484b09
Nathan GORGA <n.gorga@ad.isae-supaero.fr>
2025-06-02 17:26:28 +0200

    showing data in a way where we can visualize the blinks

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from eegDataProcessor import XDFDataUI
 import pyxdf
 import numpy as np
 
+import matplotlib.pyplot as plt
+
+
+
+"""
+the data is formatted like this :"""
+
 
 def extract_eeg_data(xdf_path):
     streams, _ = pyxdf.load_xdf(xdf_path)
@@ -14,10 +21,36 @@ def extract_eeg_data(xdf_path):
     raise RuntimeError("Aucun flux EEG trouvé dans le fichier .xdf.")
 
 
+def printOneChannel(channel_data, color, num):
+    # X-axis: sample indices (time)
+    x = np.arange(len(channel_data))
+    
+    # Y-axis: channel values (e.g., position)
+    y = channel_data[:, 1]  # or 0 depending on which value you want plotted
+
+    plt.scatter(x, y, color=color, label=f'Channel {num}', s=10)
+
+    # Draw connecting lines between consecutive points (left to right)
+    plt.plot(x, y, color=color, linewidth=0.5)
+
+    plt.xlabel("Sample Index")
+    plt.ylabel("Channel Value")
+    plt.title("Channel Signal Progression")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+    
+ 
+
 
 if __name__ == "__main__":
     path = r'C:\Users\n.gorga\code\processRawData\data\sub-P001_ses-S001_task-Default_run-001_eeg.xdf'
     raw_data = extract_eeg_data(path)
-    for data in raw_data:
-        print(data)
+  
+    printOneChannel(np.array(raw_data),"red",1)
+
+    # for data in raw_data:
+    #     print(data)
 
