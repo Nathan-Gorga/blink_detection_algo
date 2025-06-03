1655dc1b462d471ef3db6f60c5ffc8ce6cfdabf9
gorga.nathan05@gmail.com <gorga.nathan05@gmail.com>
2025-06-03 09:57:09 +0200

    create buffer start points

---
+++ buffer.py
@@ -0,0 +1,12 @@
+def bufferFullChannel(channel_data,bufferSize=150):
+
+    channelSize = len(channel_data)
+    
+    bufferStartPoints = [0]
+    check = bufferSize
+    while check <= channelSize:
+        bufferStartPoints.append(check)
+        check += bufferSize
+        
+    return bufferStartPoints
+    
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 
 from printData import extract_eeg_data,segmentChannelData,printAllChannels
+from buffer import bufferFullChannel
+
 
 if __name__ == "__main__":
     path = r'C:\Users\gorga\CodeProjects\Arduino\Blink\blink_detection_algo\data\sub-P001_ses-S001_task-Default_run-001_eeg.xdf'
     raw_data = extract_eeg_data(path)
     channels = segmentChannelData(raw_data)
-    numChannels = len(channels[0])
-    # Plot up to 4 channels in separate subplots
-    printAllChannels(channels[:numChannels])
+    bufferSize = 150
+    buffer = bufferFullChannel(channels[0],bufferSize)
+    print(buffer)
 
 
 
