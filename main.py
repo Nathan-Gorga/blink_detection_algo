28d5701abecc8b923fbd002de95bd0cf1090a5ac
Nathan GORGA <n.gorga@ad.isae-supaero.fr>
2025-06-03 15:17:23 +0200

    going to sort the signals into true and false positives and negatives

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ from buffer import bufferSizesFromChannel, bufferChannel
 from detect import detectWithThreshold
 
 if __name__ == "__main__":
-    path = r'C:\Users\n.gorga\code\processRawData\data\sample_blinks_nathan.xdf'
+    
+    path = r'C:\Users\n.gorga\code\processRawData\data\raise_eyebrows_one_second_while_blink_nathan.xdf'
     raw_data = extract_eeg_data(path)
     channels = segmentChannelData(raw_data)
     bufferSize = 10
     buffer = bufferSizesFromChannel(channels[0],bufferSize)
     
     bChannel = bufferChannel(channels[0],buffer)
-    threshold = 150
+    threshold = 150 #TODO : automatically calibrate the threshold
     isBlink = detectWithThreshold(bChannel,threshold)
 
  
